9258b2193ec5b5a267105eb85269c0c158925adc
Alan Pacheco <alan.cueva@yahoo.es>
2023-06-28 15:09:31 -0500

    Se agregan métodos para añadir pasos a las recetas e ingredientes

---
+++ microservices/recipesService.js
@@ -0,0 +1,92 @@
+const { getConnection, sql } = require("../database/dbConnection.js");
+
+const addRecipe = async (data) => {
+  const { Nombre, Duracion, UrlImagen } = data;
+  let con;
+  try {
+    con = await getConnection();
+    // Definir la consulta SQL de inserción
+    const query = `INSERT INTO Recetas (Nombre, Duracion, UrlImagen) VALUES (@nombre, @duracion, @urlImagen)`;
+
+    // Crear un objeto de solicitud de la consulta
+    const request = new sql.Request(con);
+
+    // Asignar los valores a los parámetros de la consulta
+    request.input("nombre", sql.VarChar, Nombre);
+    request.input("duracion", sql.VarChar, Duracion);
+    request.input("urlImagen", sql.VarChar, UrlImagen);
+
+    // Ejecutar la consulta SQL de inserción
+    const result = await request.query(query);
+
+    console.log("Nuevo registro insertado:", result);
+    return { status: true, content: data };
+  } catch (error) {
+    console.error("Error al insertar el registro:", error);
+    return { status: false, content: error };
+  } finally {
+    // Cerrar la conexión a la base de datos
+    await con.close();
+  }
+};
+
+const addIngredient = async (data) => {
+  const { Nombre, Unidad, RecetaID } = data;
+  let con;
+  try {
+    con = await getConnection();
+    // Definir la consulta SQL de inserción
+    const query = `INSERT INTO Ingredientes (Nombre, Unidad, RecetaID) VALUES (@nombre, @unidad, @recetaID)`;
+
+    // Crear un objeto de solicitud de la consulta
+    const request = new sql.Request(con);
+
+    // Asignar los valores a los parámetros de la consulta
+    request.input("nombre", sql.VarChar, Nombre);
+    request.input("unidad", sql.VarChar, Unidad);
+    request.input("recetaID", sql.VarChar, RecetaID);
+
+    // Ejecutar la consulta SQL de inserción
+    const result = await request.query(query);
+
+    console.log("Nuevo registro insertado:", result);
+    return { status: true, content: data };
+  } catch (error) {
+    console.error("Error al insertar el registro:", error);
+    return { status: false, content: error };
+  } finally {
+    // Cerrar la conexión a la base de datos
+    await con.close();
+  }
+};
+
+const addStep = async (data) => {
+    const { Paso, RecetaID } = data;
+    let con;
+    try {
+      con = await getConnection();
+      // Definir la consulta SQL de inserción
+      const query = `INSERT INTO Ingredientes (Paso, RecetaID) VALUES (@paso, @recetaID)`;
+  
+      // Crear un objeto de solicitud de la consulta
+      const request = new sql.Request(con);
+  
+      // Asignar los valores a los parámetros de la consulta
+      request.input("Paso", sql.VarChar, Paso);
+      request.input("recetaID", sql.VarChar, RecetaID);
+  
+      // Ejecutar la consulta SQL de inserción
+      const result = await request.query(query);
+  
+      console.log("Nuevo registro insertado:", result);
+      return { status: true, content: data };
+    } catch (error) {
+      console.error("Error al insertar el registro:", error);
+      return { status: false, content: error };
+    } finally {
+      // Cerrar la conexión a la base de datos
+      await con.close();
+    }
+  };
+
+module.exports = { addRecipe, addIngredient, addStep };
--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { getAllByTable } = require("../database/generalMethods");
+const {
+  addRecipe,
+  addIngredient,
+  addStep,
+} = require("../microservices/recipesService.js");
 
 // Ruta para obtener todos los usuarios
 router.get("", async (req, res) => {
@@ -15,6 +20,52 @@ router.get("", async (req, res) => {
     res.status(200).json(response.content.recordset);
   }
 });
+
+// Ruta para crear un nuevo usuario
+router.post("", async (req, res) => {
+  const data = req.body; // Acceder correctamente al cuerpo de la solicitud
+
+  const response = await addRecipe(data);
+  // Lógica para crear un nuevo usuario
+  if (!response.status) {
+    res.status(500).json({
+      error: "Error al ejecutar la consulta post",
+      response: response.content,
+    });
+  } else {
+    res.status(200).json({ data });
+  }
+});
+
+router.post("/ingredients", async (req, res) => {
+  const data = req.body; // Acceder correctamente al cuerpo de la solicitud
+
+  const response = await addIngredient(data);
+  // Lógica para crear un nuevo usuario
+  if (!response.status) {
+    res.status(500).json({
+      error: "Error al ejecutar la consulta post",
+      response: response.content,
+    });
+  } else {
+    res.status(200).json({ data });
+  }
+});
+
+router.post("/steps", async (req, res) => {
+    const data = req.body; // Acceder correctamente al cuerpo de la solicitud
+  
+    const response = await addStep(data);
+    // Lógica para crear un nuevo usuario
+    if (!response.status) {
+      res.status(500).json({
+        error: "Error al ejecutar la consulta post",
+        response: response.content,
+      });
+    } else {
+      res.status(200).json({ data });
+    }
+  });
 
 // Exporta el objeto router
 module.exports = router;
