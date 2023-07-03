e54e04048badfb76a7194603520077bb5a2a3e0c
Alan Pacheco <alan.cueva@yahoo.es>
2023-07-02 21:29:07 -0500

    Se agrega método para listar pasos

--- routes/recipesRoutes.js
+++ routes/recipesRoutes.js
@@ -64,6 +64,19 @@ router.get("/ingredients", async (req, res) => {
   }
 });
 
+router.get("/steps", async (req, res) => {
+  const response = await getAllByTable("Pasos");
+  res.setHeader("Content-Type", "application/json");
+  if (!response.status) {
+    res.status(500).json({
+      error: "Error al ejecutar la consulta get ",
+      response: response.content,
+    });
+  } else {
+    res.status(200).json(response.content.recordset);
+  }
+});
+
 // Ruta para crear un nuevo usuario
 router.post("", async (req, res) => {
   const data = req.body; // Acceder correctamente al cuerpo de la solicitud
